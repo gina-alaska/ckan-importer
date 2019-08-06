0bc885a0f742680391c2ee230ab40383c01e8627
Craig Stephenson <crstephenson@alaska.edu>
2019-08-05 22:45:16 -0800

    Demonstrate how to set spatial GeoJSON field and other custom fields from ckanext-glynx module through API.

--- import.py
+++ import.py
@@ -15,8 +15,16 @@ print org
 
 # Create a new Dataset.
 pkg = site.action.package_create(
-    name='example-dataset',
-    title='Example Dataset',
+    title='Prudhoe Bay Map B',
+    notes='Map B shapefile with all layers combined.',
+    name='prudhoe_bay_map_b',
+    status='Complete',                  # Dropdown
+    archived_at='2019-08-06T05:39:42',  # Date
+    request_contact_info='true',        # Boolean
+    extras=[{                           # GeoJSON
+        'key': 'spatial',
+        'value': '{"type": "Polygon", "coordinates": [[[-162.0703125, 69.47296854140573], [-148.88671875, 69.47296854140573], [-148.88671875, 72.3424643905499], [-162.0703125, 72.3424643905499], [-162.0703125, 69.47296854140573]]]}'
+    }],
     owner_org='example-organization'
 )
 print pkg
@@ -24,7 +32,7 @@ print pkg
 # Attach a URL as a resource to an existing Dataset.
 url_res = site.action.resource_create(
      name='example-resource-url',
-     package_id='example-dataset',
+     package_id='prudhoe_bay_map_b',
      url='http://example.com'
 )
 print url_res
@@ -32,7 +40,7 @@ print url_res
 # Upload a file resource to DataStore and attach it to an existing Dataset.
 file_res = site.action.resource_create(
     name='example-resource-file',
-    package_id='example-dataset',
+    package_id='prudhoe_bay_map_b',
     upload=open('/path/to/file', 'rb')
 )
 print file_res
