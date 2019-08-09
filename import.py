aa1c0a5529a99a4f9a7c068b5e2a740b2a7960c4
Craig Stephenson <crstephenson@alaska.edu>
2019-08-08 22:24:35 -0800

    Populate status vocab tags from status.json file.

--- import.py
+++ import.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 import ckanapi
+import json
 
 site = ckanapi.RemoteCKAN(
     'http://ckan.url',
     apikey='...' # Found on CKAN user page.
 )
 
+voc_response = site.action.vocabulary_create(
+    name='status'
+)
+print voc_response
+
+# Read and create Status tags from status.json.
+with open('status.json') as status_file:
+    data = json.load(status_file)
+    for tag in data['status']:
+        response = site.action.tag_create(
+            name=tag,
+            vocabulary_id=voc_response['id']
+        )
+        print response
+
 # Create a new Organization.
-org = site.action.organization_create(
+response = site.action.organization_create(
     name='example-organization',
     title='Example Organization'
 )
-print org
+print response
 
 # Create a new Dataset.
-pkg = site.action.package_create(
+response = site.action.package_create(
     title='Prudhoe Bay Map B',
     notes='Map B shapefile with all layers combined.',
     name='prudhoe_bay_map_b',
@@ -27,20 +43,20 @@ pkg = site.action.package_create(
     }],
     owner_org='example-organization'
 )
-print pkg
+print response
 
 # Attach a URL as a resource to an existing Dataset.
-url_res = site.action.resource_create(
+response = site.action.resource_create(
      name='example-resource-url',
      package_id='prudhoe_bay_map_b',
-     url='http://example.com'
+     url='https://uaf.edu'
 )
-print url_res
+print response
 
 # Upload a file resource to DataStore and attach it to an existing Dataset.
-file_res = site.action.resource_create(
+response = site.action.resource_create(
     name='example-resource-file',
     package_id='prudhoe_bay_map_b',
-    upload=open('/path/to/file', 'rb')
+    upload=open('/Users/cstephenson/Downloads/UAFLogo_A_647.pdf', 'rb')
 )
-print file_res
+print response
